6a54dbb2e8073ebd81275364f86080157cdd1979
rayankolkas <rayankolkas04@gmail.com>
2025-05-27 11:56:32 +0300

    moving them

---
+++ src/main/java/com/example/demo/Course.java
@@ -0,0 +1,26 @@
+package com.example;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Course {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String courseName;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+
+    private LocalDateTime lastModifiedAt;
+    private String lastModifiedBy;
+
+    @ManyToOne
+    private Department department;
+}
